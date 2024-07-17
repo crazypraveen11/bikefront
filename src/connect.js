06ef21d595e69db8aacedd51b5607dfbd743cda2
Praveensenthil <praveensenthil1173@gmail.com>
2024-07-17 14:33:04 +0530

    new

--- src/connect.js
+++ src/connect.js
@@ -7,13 +7,14 @@ export let Loginprocess = async (object) => {
         const totalvalue = btoa(twovalues);
 
         const temp = await axios.get(`http://localhost:8081/indianbank/`,
-            {
+            { 
                 headers:
                 {
                     "Authorization": `Basic ${totalvalue}`
                 }
-            }
+            } 
         )
+        
 
         if (temp) {
             sessionStorage.setItem("bikesecurity", object.bikeBrand);
